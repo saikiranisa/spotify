747605047a7e48aeed86ae971734ef9672c47d3f
Isa saikiran <88225022+saikiranisa@users.noreply.github.com>
2022-09-09 17:15:53 +0530

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/NavBar.css";
+
+const NavBar = () => {
+    return(
+        <div className="navbar">
+            <i className="fab fa-spotify"></i>
+            <div className="app-header">Spotify</div>
+            <div className="nav-links">
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
